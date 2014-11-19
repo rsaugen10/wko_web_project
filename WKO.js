0933295d3384f547b87fba707e42e88d89e81f4a
NMartin11 <NMartin11@winona.edu>
2014-11-19 14:38:48 -0600

    This is JavaScript file for the slide show

---
+++ WKO.js
@@ -0,0 +1,44 @@
+/*
+--- Scroll through pictures
+*/
+var slideImages = new Array();
+slideImages[0] = new Image();
+slideImages[0].src = "wko.jpg";
+slideImages[1] = new Image();
+slideImages[1].src = "Reaching_Hand.jpg";
+
+var step=0;
+function slideit(){
+
+ setTimeout(function() {
+  $("#mainPic").attr("src", slideImages[step].src).hide().fadeIn();
+ });
+
+  if (step<1)
+ {
+  step++;
+ }
+ else{
+  step=0;
+	}
+ setTimeout("slideit()",5000);
+ };
+ 
+slideit();
+
+/*
+--------- END!! ------------
+*/
+
+
+/*
+--- Scroll through pictures
+*/
+
+
+
+
+
+/*
+--------- END!! ------------
+*/
