4da735f4bfd619a14cba1d5bb861f201c5405fe4
rsaugen10 <rsaugen10@winona.edu>
2014-11-24 12:52:15 -0600

    News.php
    Adding in news .php

---
+++ news.php
@@ -0,0 +1,51 @@
+
+<html>
+<head>
+ <title> index</title>
+ <link rel="stylesheet" type="text/css" href="project.css">
+</head>
+
+
+
+
+<body>
+
+<div class = "wrapper"> 
+		<div class = "banner">
+		</div>
+		<div class = "menu">
+			<ul>
+				<li><a href="pages.php?pages=About Us">About Us</a></li>
+				<li><a href="pages.php?pages=Our Programs">Our Programs</a></li>
+				<li><a href="pages.php?pages=Help Now">Help Now</a></li>
+				<li><a href="news.php">News</a></li>
+				<li><a href="Contact.html">Contact</a></li>
+			</ul>
+			
+		</div>
+	<h2 id="recent_news"> Recent News</h2>
+	
+</div>
+
+	
+<div id="disqus_container">
+<div id="disqus_thread">
+	<div id="disqus">
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'wko'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	</div>
+</div>
+</div>
+</body>
+
+</html>
