7fa709e628496175774a28e368ec65dbd924b393
pmcphail12 <pmcphail12@winona.edu>
2014-12-04 17:14:52 -0600

    menu change

---
+++ menu.php
@@ -0,0 +1,13 @@
+<?php echo 
+'<a href ="index.php"><div class = "banner">
+</div></a>
+		<div class = "menu">
+			<ul>
+				<li class = "linkmenu"><a href="aboutus.php?pages=About Us">About Us</a></li>
+				<li class = "linkmenu"><a href="ourprograms.php?pages=Our Programs">Our Programs</a></li>
+				<li class = "linkmenu"><a href="help.php?pages=Help Now">Help Now</a></li>
+				<li class = "linkmenu"><a href="news.php">News</a></li>
+				<li class = "linkmenu"><a href="contact.php">Contact Us</a></li>
+			</ul>
+		</div>'
+?>
