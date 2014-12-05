0155a5d3ed5f29a5a52ffcef2d6507a9b7f68d06
rsaugen10 <rsaugen10@winona.edu>
2014-12-04 20:50:38 -0600

    Updating news.php and CSS
    Still need to move Facebook feed up

--- news.php
+++ news.php
@@ -27,14 +27,15 @@
 			
 		</div>	
 		
-		
-		<div class="recent_news">
-			<h1> Recent News </h1>
+	<div class="recent_news_wrapper">
+	<div class="recent_news">
+			<br>
+			<h2> Recent News </h2>
 			<br>
 			<br>
-			<h2> Share A Toy </h2>
+			<h3> Share A Toy </h3>
 			<br>
-			<p="news_info">
+			<p>
 			Each year, Whole Kids Outreach hosts its Share A Toy Christmas Store
  			to provide families in need a place to shop for their children’s Christmas 
  			gifts. Parents in the WKO region are welcome to access this Store to 
@@ -52,32 +53,48 @@
 			 are accepted throughout the year with final collections during the 
 			 last week of November when possible.
 			</p>	
+			</div>
 			<br>
 			<br>
-			
-			<button class="showDisqus" id="showDisqus">showDisqus</button>
 		</div>
 </div>
   
-    
-    
     <div id="disqus_container">
-
-
-
-
-
-
-<div id="disqus_thread">
+    <div id="disqus_thread">
 	<div id="disqus">
-	
-	
-    
-    
-    
+
+		<script>
+			 var disqus_shortname = 'wko'; // required: replace example with your forum shortname
+
+     			   /* * * DON'T EDIT BELOW THIS LINE * * */
+   			     (function disqus() 
+     			   {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+      			  
+   				     var disqusPublicKey = "REPLACE_WITH_DISQUS_PUBLIC_KEY";
+				var disqusShortname = "REPLACE_WITH_DISQUS_SHORTNAME";
+				var threadUrl = 'link:' + $('.show-comments').attr('data-disqus-url');
+
+				</script>
+
+ 
 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 	</div>
 </div>
+
+		<div id = "feed">
+				<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2F
+				www.facebook.com%2Fnonprofits&amp;width=490&amp;colorscheme=light&amp;show_faces=true
+				&amp;border_color&amp;stream=true&amp;header=true&amp;height=435" scrolling="yes" frameborder="0" 
+				style="border:none; overflow:hidden; width:490px; height:78%; background: white; float:left; 
+				"allowTransparency="true"></iframe>
+		</div>
+
+
+
 </div>
 </body>
 
